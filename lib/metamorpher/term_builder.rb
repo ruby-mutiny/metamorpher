0583ad367b106ebc2c1de246e5e90408903d6854
Louis Rose <louismrose@gmail.com>
2014-04-11 19:40:05 +0100

    Add first version of term builder, which provides syntactic sugar for creating subclasses of Node.

---
+++ lib/metamorpher/term_builder.rb
@@ -0,0 +1,44 @@
+require "metamorpher/literal"
+require "metamorpher/variable"
+
+module Metamorpher
+  class TermBuilder
+    def literal!(name, *children)
+      Literal.new(name: name, children: children.map { |c| nodify(c) })
+    end
+
+    def variable!(name, &block)
+      if block
+        Variable.new(name: name, condition: block)
+      else
+        Variable.new(name: name)
+      end
+    end
+
+    def greedy_variable!(name, &block)
+      if block
+        Variable.new(name: name, condition: block, greedy?: true)
+      else
+        Variable.new(name: name, greedy?: true)
+      end
+    end
+
+    def method_missing(method, *arguments, &block)
+      if method.to_s.start_with?("_") && !arguments.empty? && arguments.first == :greedy
+        greedy_variable!(method[1..-1].to_sym, *arguments[1..-1], &block)
+
+      elsif method.to_s.start_with?("_")
+        variable!(method[1..-1].to_sym, *arguments, &block)
+
+      else
+        literal!(method, *arguments)
+      end
+    end
+
+    private
+
+    def nodify(item)
+      item.kind_of?(Node) ? item : literal!(item)
+    end
+  end
+end
---
+++ spec/metamorpher/term_builder_spec.rb
@@ -0,0 +1,167 @@
+require "metamorpher/term_builder"
+
+module Metamorpher
+  describe TermBuilder do
+    describe "literal!" do
+      it "should create an instance of Literal" do
+        actual = subject.literal!(:a)
+        expected = Literal.new(name: :a)
+
+        expect(actual).to eq(expected)
+      end
+
+      it "should work with numeric names" do
+        actual = subject.literal!(4)
+        expected = Literal.new(name: 4)
+
+        expect(actual).to eq(expected)
+      end
+
+      it "should capture single children" do
+        actual = subject.literal!(:inc, subject.literal!(1))
+        expected = Literal.new(name: :inc, children: [Literal.new(name: 1)])
+
+        expect(actual).to eq(expected)
+      end
+
+      it "should capture several children" do
+        actual = subject.literal!(:add, subject.literal!(1), subject.literal!(2))
+        expected = Literal.new(
+          name: :add,
+          children: [
+            Literal.new(name: 1),
+            Literal.new(name: 2)
+          ]
+        )
+
+        expect(actual).to eq(expected)
+      end
+
+      it "should automatically convert children to literals" do
+        actual = subject.literal!(:add, 1, 2)
+        expected = Literal.new(
+          name: :add,
+          children: [
+            Literal.new(name: 1),
+            Literal.new(name: 2)
+          ]
+        )
+
+        expect(actual).to eq(expected)
+      end
+
+      it "should not automatically convert children that are already nodes" do
+        actual = subject.literal!(:inc, subject.variable!(:a))
+        expected = Literal.new(name: :inc, children: [Variable.new(name: :a)])
+
+        expect(actual).to eq(expected)
+      end
+    end
+
+    describe "literal shorthand" do
+      it "should create an instance of Literal" do
+        actual = subject.a
+        expected = Literal.new(name: :a)
+
+        expect(actual).to eq(expected)
+      end
+
+      it "should capture children" do
+        actual = subject.add(1, 2)
+        expected = Literal.new(
+          name: :add,
+          children: [
+            Literal.new(name: 1),
+            Literal.new(name: 2)
+          ]
+        )
+
+        expect(actual).to eq(expected)
+      end
+    end
+
+    describe "variable!" do
+      it "should create an instance of Variable" do
+        actual = subject.variable!(:a)
+        expected = Variable.new(name: :a)
+
+        expect(actual).to eq(expected)
+      end
+
+      it "should create condition from block" do
+        built = subject.variable!(:a) { |node| node > 0 }
+
+        expect(built.name).to eq(:a)
+        expect(built.condition.call(1)).to be_true
+        expect(built.condition.call(-1)).to be_false
+      end
+
+      it "should not allow children" do
+        expect { subject.variable!(:inc, 1) }.to raise_error
+      end
+    end
+
+    describe "variable shorthand" do
+      it "should create an instance of Variable" do
+        actual = subject._a
+        expected = Variable.new(name: :a)
+
+        expect(actual).to eq(expected)
+      end
+
+      it "should create condition from block" do
+        built = subject._a { |node| node > 0 }
+
+        expect(built.name).to eq(:a)
+        expect(built.condition.call(1)).to be_true
+        expect(built.condition.call(-1)).to be_false
+      end
+
+      it "should not allow children" do
+        expect { subject._inc(1) }.to raise_error
+      end
+    end
+
+    describe "greedy_variable!" do
+      it "should create an instance of Variable with greedy? set to true" do
+        actual = subject.greedy_variable!(:a)
+        expected = Variable.new(name: :a, greedy?: true)
+
+        expect(actual).to eq(expected)
+      end
+
+      it "should create condition from block" do
+        built = subject.greedy_variable!(:a) { |node| node > 0 }
+
+        expect(built.name).to eq(:a)
+        expect(built.condition.call(1)).to be_true
+        expect(built.condition.call(-1)).to be_false
+      end
+
+      it "should not allow children" do
+        expect { subject.greedy_variable!(:inc, 1) }.to raise_error
+      end
+    end
+
+    describe "greedy variable shorthand" do
+      it "should create an instance of Variable with greedy? set to true" do
+        actual = subject._a :greedy
+        expected = Variable.new(name: :a, greedy?: true)
+
+        expect(actual).to eq(expected)
+      end
+
+      it "should create condition from block" do
+        built = subject._a(:greedy) { |node| node > 0 }
+
+        expect(built.name).to eq(:a)
+        expect(built.condition.call(1)).to be_true
+        expect(built.condition.call(-1)).to be_false
+      end
+
+      it "should not allow children" do
+        expect { subject._inc(:greedy, 1) }.to raise_error
+      end
+    end
+  end
+end
