c4e37c663b8809022d7594e18498f896a252f815
Louis Rose <louismrose@gmail.com>
2014-04-29 19:09:18 +0100

    Be more concise in default builder spec.

--- spec/integration/default/builder_spec.rb
+++ spec/integration/default/builder_spec.rb
@@ -1,6 +1,6 @@
 require "metamorpher"
 
-describe Metamorpher::Builders::Default::Builder do
+describe Metamorpher.builder do
   it_behaves_like "a literal builder"
   it_behaves_like "a variable builder"
   it_behaves_like "a greedy variable builder"
