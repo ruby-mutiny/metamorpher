0632327ea37f23319a7af179c7b30769ada2ff90
Louis Rose <louismrose@gmail.com>
2014-04-29 17:01:42 +0100

    Ensure that Metamorpher::Refactorer is required by default.

--- lib/metamorpher.rb
+++ lib/metamorpher.rb
@@ -2,6 +2,7 @@ require "metamorpher/version"
 require "metamorpher/builder"
 require "metamorpher/matcher"
 require "metamorpher/rewriter"
+require "metamorpher/refactorer"
 
 module Metamorpher
   def self.builder
