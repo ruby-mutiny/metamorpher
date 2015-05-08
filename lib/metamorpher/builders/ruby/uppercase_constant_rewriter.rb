100c596033973437e65356b658c9507ebeb5a93a
Louis Rose <louismrose@gmail.com>
2015-05-09 00:45:55 +0100

    Reduce duplication in Ruby builder code.

--- lib/metamorpher/builders/ruby/uppercase_constant_rewriter.rb
+++ lib/metamorpher/builders/ruby/uppercase_constant_rewriter.rb
@@ -1,29 +1,15 @@
 require "metamorpher/builders/ast"
+require "metamorpher/builders/ruby/uppercase_rewriter"
 
 module Metamorpher
   module Builders
     module Ruby
-      class UppercaseConstantRewriter
+      class UppercaseConstantRewriter < UppercaseRewriter
         include Metamorpher::Rewriter
         include Metamorpher::Builders::AST
 
         def pattern
-          builder.const(
-            builder.literal!(nil),
-            builder.VARIABLE_TO_BE { |v| v.name && v.name.to_s[/^[A-Z_]*$/] }
-          )
-        end
-
-        def replacement
-          builder.derivation!(:variable_to_be) do |variable_to_be, builder|
-            name = variable_to_be.name.to_s
-
-            if name.end_with?("_")
-              builder.greedy_variable! name.chomp("_").downcase.to_sym
-            else
-              builder.variable! name.downcase.to_sym
-            end
-          end
+          builder.const(builder.literal!(nil), super)
         end
       end
     end
