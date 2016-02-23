bfa3c81a5e71925d26a80630250e796d5f22595f
Louis Rose <louismrose@gmail.com>
2016-02-23 16:54:21 +0000

    Fix bug in computing paths of TermSets.

--- lib/metamorpher/terms/term_set.rb
+++ lib/metamorpher/terms/term_set.rb
@@ -5,11 +5,20 @@ module Metamorpher
     class TermSet < Term
       attributes terms: []
 
+      def initialize(attributes = {})
+        initialize_attributes(attributes)
+        terms.each { |term| term.parent = self }
+      end
+
       def inspect
         "TermSet" + terms.inspect
       end
 
       def alternatives
+        terms
+      end
+
+      def children
         terms
       end
     end
